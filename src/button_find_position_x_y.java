89ab40140bc76734cb589446a8ff274a92c2ab63
yhalk <galkevychyura@gmail.com>
2021-11-09 22:49:29 +0200

    qwe

--- src/button_find_position_x_y.java
+++ src/button_find_position_x_y.java
@@ -1,5 +1,7 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class button_find_position_x_y {
@@ -12,8 +14,16 @@ public class button_find_position_x_y {
         //find and click main Button
         driver.findElement(By.xpath("//*[@id=\"post-153\"]/div[2]/div/ul/li[2]/a/img")).click();
         Thread.sleep(4000);
-        //find and click button go to home page
-        driver.findElement(By.xpath("//*[@id=\"home\"]")).click();
+        //find button position and size
+        WebElement element = driver.findElement(By.id("position"));
+        Point location = element.getLocation();
+        int x = location.getX();
+        int y = location.getX();
+        double elementWidth = element.getSize().getWidth();
+        double elementHight = element.getSize().getHeight();
+        System.out.println("Coordinates: x= " + x + " and y= " + y);
+        System.out.println("Width: " + elementWidth + " and Hight " + elementHight);
+        System.out.println(element.getText());
         Thread.sleep(4000);
         driver.close();
 
